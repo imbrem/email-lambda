045b397ce1613ad49c3f7ea031a12e65f3a2e59f
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2024-01-02 21:04:06 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use aws_config::BehaviorVersion;
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
+
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+async fn function_handler(
+    _event: Request,
+    _client: &aws_sdk_sesv2::Client,
+) -> Result<Response<Body>, Error> {
+    let message = format!("Not implemented");
+
+    // Return something that implements IntoResponse.
+    // It will be serialized to the right response event automatically by the runtime
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let client = aws_sdk_sesv2::Client::new(&config);
+
+    run(service_fn(|event| function_handler(event, &client))).await
+}
